30fb53619a9d70829ff45ee7103b1f0418748e67
Sumanth Banka <bankasumanth@gmail.com>
2022-07-11 13:39:07 +0530

    Create grid.py

---
+++ GUI/grid.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+# grid() = geometry manager that organizes widgets in a table like structure in a 
+
+window  = Tk()
+
+firstnameLabel = Label(window,text="First name:").grid(row=0,column=0)
+firstnameEntry = Entry(window).grid(row=0,column=1)
+
+lastnameLabel = Label(window,text="First name:").grid(row=1,column=0)
+lastnameEntry = Entry(window).grid(row=1,column=1)
+
+emailLabel = Label(window,text="First name:").grid(row=2,column=0)
+emailEntry = Entry(window).grid(row=2,column=1)
+
+submitbutton = Button(window,text="SUBMIT").grid()
+
+window.mainloop()
